89e37bcccc5d647fa99281a7d1696792b55c8c02
JoshMills-fai <josh@fai2.com>
2022-08-14 19:38:40 -0400

    filter results based on user input, display in template

--- main.go
+++ main.go
@@ -30,66 +30,70 @@ type Members struct {
 	Status    string `json:"status"`
 	Copyright string `json:"copyright"`
 	Results   []struct {
-		Congress    string `json:"congress"`
-		Chamber     string `json:"chamber"`
-		NumResults  int    `json:"num_results"`
-		Offset      int    `json:"offset"`
-		Memberslist []struct {
-			ID                   string      `json:"id"`
-			Title                string      `json:"title"`
-			ShortTitle           string      `json:"short_title"`
-			APIURI               string      `json:"api_uri"`
-			FirstName            string      `json:"first_name"`
-			MiddleName           interface{} `json:"middle_name"`
-			LastName             string      `json:"last_name"`
-			Suffix               interface{} `json:"suffix"`
-			DateOfBirth          string      `json:"date_of_birth"`
-			Gender               string      `json:"gender"`
-			Party                string      `json:"party"`
-			LeadershipRole       interface{} `json:"leadership_role"`
-			TwitterAccount       string      `json:"twitter_account"`
-			FacebookAccount      string      `json:"facebook_account"`
-			YoutubeAccount       string      `json:"youtube_account"`
-			GovtrackID           string      `json:"govtrack_id"`
-			CspanID              string      `json:"cspan_id"`
-			VotesmartID          string      `json:"votesmart_id"`
-			IcpsrID              string      `json:"icpsr_id"`
-			CrpID                string      `json:"crp_id"`
-			GoogleEntityID       string      `json:"google_entity_id"`
-			FecCandidateID       string      `json:"fec_candidate_id"`
-			URL                  string      `json:"url"`
-			RssURL               string      `json:"rss_url"`
-			ContactForm          string      `json:"contact_form"`
-			InOffice             bool        `json:"in_office"`
-			CookPvi              interface{} `json:"cook_pvi"`
-			DwNominate           float64     `json:"dw_nominate"`
-			IdealPoint           interface{} `json:"ideal_point"`
-			Seniority            string      `json:"seniority"`
-			NextElection         string      `json:"next_election"`
-			TotalVotes           int         `json:"total_votes"`
-			MissedVotes          int         `json:"missed_votes"`
-			TotalPresent         int         `json:"total_present"`
-			LastUpdated          string      `json:"last_updated"`
-			OcdID                string      `json:"ocd_id"`
-			Office               string      `json:"office"`
-			Phone                string      `json:"phone"`
-			Fax                  string      `json:"fax"`
-			State                string      `json:"state"`
-			SenateClass          string      `json:"senate_class"`
-			StateRank            string      `json:"state_rank"`
-			LisID                string      `json:"lis_id"`
-			MissedVotesPct       float64     `json:"missed_votes_pct"`
-			VotesWithPartyPct    float64     `json:"votes_with_party_pct"`
-			VotesAgainstPartyPct float64     `json:"votes_against_party_pct"`
-		} `json:"members"`
+		Congress    string        `json:"congress"`
+		Chamber     string        `json:"chamber"`
+		NumResults  int           `json:"num_results"`
+		Offset      int           `json:"offset"`
+		Memberslist []Memberslist `json:"members"`
 	} `json:"results"`
 }
 
-type State struct {
+type Memberslist struct {
+	ID                   string      `json:"id"`
+	Title                string      `json:"title"`
+	ShortTitle           string      `json:"short_title"`
+	APIURI               string      `json:"api_uri"`
+	FirstName            string      `json:"first_name"`
+	MiddleName           interface{} `json:"middle_name"`
+	LastName             string      `json:"last_name"`
+	Suffix               interface{} `json:"suffix"`
+	DateOfBirth          string      `json:"date_of_birth"`
+	Gender               string      `json:"gender"`
+	Party                string      `json:"party"`
+	LeadershipRole       interface{} `json:"leadership_role"`
+	TwitterAccount       string      `json:"twitter_account"`
+	FacebookAccount      string      `json:"facebook_account"`
+	YoutubeAccount       string      `json:"youtube_account"`
+	GovtrackID           string      `json:"govtrack_id"`
+	CspanID              string      `json:"cspan_id"`
+	VotesmartID          string      `json:"votesmart_id"`
+	IcpsrID              string      `json:"icpsr_id"`
+	CrpID                string      `json:"crp_id"`
+	GoogleEntityID       string      `json:"google_entity_id"`
+	FecCandidateID       string      `json:"fec_candidate_id"`
+	URL                  string      `json:"url"`
+	RssURL               string      `json:"rss_url"`
+	ContactForm          string      `json:"contact_form"`
+	InOffice             bool        `json:"in_office"`
+	CookPvi              interface{} `json:"cook_pvi"`
+	DwNominate           float64     `json:"dw_nominate"`
+	IdealPoint           interface{} `json:"ideal_point"`
+	Seniority            string      `json:"seniority"`
+	NextElection         string      `json:"next_election"`
+	TotalVotes           int         `json:"total_votes"`
+	MissedVotes          int         `json:"missed_votes"`
+	TotalPresent         int         `json:"total_present"`
+	LastUpdated          string      `json:"last_updated"`
+	OcdID                string      `json:"ocd_id"`
+	Office               string      `json:"office"`
+	Phone                string      `json:"phone"`
+	Fax                  string      `json:"fax"`
+	State                string      `json:"state"`
+	SenateClass          string      `json:"senate_class"`
+	StateRank            string      `json:"state_rank"`
+	LisID                string      `json:"lis_id"`
+	MissedVotesPct       float64     `json:"missed_votes_pct"`
+	VotesWithPartyPct    float64     `json:"votes_with_party_pct"`
+	VotesAgainstPartyPct float64     `json:"votes_against_party_pct"`
+}
+
+type MyState struct {
 	Abbreviation string
 }
 
+// ///////////////////////////////////////////////TODO Rename these
 var members Members
+var mymembers Members
 
 func GetJson(url string) []byte {
 
@@ -110,6 +114,7 @@ func GetJson(url string) []byte {
 
 }
 
+// ///////////////////////////////TODO differentiate between calling external api and ours
 func getMembers(c *gin.Context) {
 
 	// return []byte
@@ -123,7 +128,6 @@ func getMembers(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusOK, members)
-
 }
 
 func home(c *gin.Context) {
@@ -132,19 +136,37 @@ func home(c *gin.Context) {
 	tmpl.Execute(c.Writer, nil)
 }
 
-func handleFormSubmit(c *gin.Context) {
+func myrepresentatives(c *gin.Context) {
+	//Consume from our own API
+	////////////////////////////////////////////////Todo, make this url dynamic
+	res, _ := http.Get("http://localhost:3000/api/members")
+	data, _ := ioutil.ReadAll(res.Body)
+
+	json.Unmarshal(data, &mymembers)
 
 	var tmpl *template.Template
 	tmpl = template.Must(template.ParseFiles("templates/my-representatives.html"))
 
 	c.Request.ParseForm()
-	var state State = State{c.Request.Form.Get("state")}
 
-	if len(state.Abbreviation) == 0 {
+	var UserState MyState = MyState{c.Request.Form.Get("state")}
+
+	if len(UserState.Abbreviation) == 0 {
 		fmt.Println("no state")
 		c.Redirect(301, "/")
 	} else {
-		tmpl.Execute(c.Writer, state)
+
+		var memberMatch []Memberslist
+		//filter by user state
+		for _, member := range mymembers.Results[0].Memberslist {
+			if member.State == UserState.Abbreviation {
+				memberMatch = append(memberMatch, member)
+			}
+		}
+
+		fmt.Printf("%+v\n", memberMatch)
+
+		tmpl.Execute(c.Writer, memberMatch)
 	}
 
 }
@@ -154,7 +176,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", home)
 	router.GET("/api/members", getMembers)
-	router.POST("/my-representatives", handleFormSubmit)
+	router.POST("/my-representatives", myrepresentatives)
 	router.Run(":3000")
 
 }
