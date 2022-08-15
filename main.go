36beb2d77a29367a4a6cf52d942596ac245860fc
JoshMills-fai <josh@fai2.com>
2022-08-14 20:53:16 -0400

    serve template, parse form before redirecting

--- main.go
+++ main.go
@@ -137,11 +137,6 @@ func home(c *gin.Context) {
 }
 
 func myRepresentatives(c *gin.Context) {
-	if len(UserState.Abbreviation) == 0 {
-		fmt.Println("no state")
-		c.Redirect(301, "/")
-	}
-
 	//Consume from our own API
 	////////////////////////////////////////////////Todo, make this url dynamic
 	res, _ := http.Get("http://localhost:3000/api/members")
@@ -154,15 +149,23 @@ func myRepresentatives(c *gin.Context) {
 
 	c.Request.ParseForm()
 
-	var memberMatch []Memberslist
-	//filter by user state
-	for _, member := range mymembers.Results[0].Memberslist {
-		if member.State == UserState.Abbreviation {
-			memberMatch = append(memberMatch, member)
-		}
-	}
+	var UserState MyState = MyState{c.Request.Form.Get("state")}
 
-	tmpl.Execute(c.Writer, memberMatch)
+	if len(UserState.Abbreviation) == 0 {
+		fmt.Println("no state")
+		c.Redirect(301, "/")
+	} else {
+
+		var memberMatch []Memberslist
+		//filter by user state
+		for _, member := range mymembers.Results[0].Memberslist {
+			if member.State == UserState.Abbreviation {
+				memberMatch = append(memberMatch, member)
+			}
+		}
+
+		tmpl.Execute(c.Writer, memberMatch)
+	}
 
 }
 
